be9cbfef83121d2ac17ea1b8fa2e7c60bfc37cd8
Meshack Bwire <basilmeshack13@gmail.com>
2023-09-14 15:46:22 +0300

    Nested loops continuation

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * file: 2-mul.c
+ * auth: joshua ndungu
+ */
+
+#include "main.h"
+
+/**
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+ 
